0a69cddfeeb7f4dad2e70030402b90b26cbb7280
LapoFrati <fratilapo@gmail.com>
2015-03-22 20:13:48 +0000

    Move Mocks to test folder.

--- Scrabble/test/scrabBot/MockScrabble.java
+++ Scrabble/test/scrabBot/MockScrabble.java
@@ -7,7 +7,6 @@ public class MockScrabble extends Scrabble {
 		P2 = new MockPlayer();
 		board = new MockBoard();
 		pool = new MockPool();
-		scoreP1 = scoreP2 = 0;
 		activePlayer = P1;
 		ui = new UI();
 		keepPlaying = true;
