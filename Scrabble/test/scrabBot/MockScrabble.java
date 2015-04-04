dec5cdb34d5e55e82ed2fde2c96044c7a6ad9aa6
LapoFrati <fratilapo@gmail.com>
2015-04-04 22:33:48 +0100

    Update MockScrabble.

--- Scrabble/test/scrabBot/MockScrabble.java
+++ Scrabble/test/scrabBot/MockScrabble.java
@@ -6,11 +6,17 @@ package scrabBot;
 public class MockScrabble extends Scrabble {
 	
 	public MockScrabble(){
-		P1 = new MockPlayer();
-		P2 = new MockPlayer();
+		turn = new Player[2];
+		turn[0] = new Player();
+		turn[0].setPlayerName("Lapo");
+		turn[0].getPlayerFrame().refillFrame(pool);
+		turn[1] = new Player();
+		turn[1].setPlayerName("Simone");
+		turn[1].getPlayerFrame().refillFrame(pool);
 		board = new MockBoard();
 		pool = new MockPool();
-		activePlayer = P1;
+		activePlayer = turn[0];
+		currentPlayerNumber = 0;
 		ui = new UI(System.in);
 		keepPlaying = true;
 		dict = new Dictionary();
