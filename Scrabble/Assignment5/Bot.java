f6977ea2cd577e43f1f1338769dee95058bee284
LapoFrati <fratilapo@gmail.com>
2015-04-10 09:35:21 +0100

    Add Assignment5 code in a separate folder.

---
+++ Scrabble/Assignment5/Bot.java
@@ -0,0 +1,32 @@
+
+public class Bot {
+
+	private Word word = new Word();
+	private String letters;
+	
+	Bot () {
+		word.setWord(0, 0, Word.HORIZONTAL, "HELLO");
+		letters = "XYZ";
+	}
+	
+	public int getCommand (Player player, Board board) {
+		// make a decision on the play here
+		// use board.getSqContents to check what is on the board
+		// use Board.SQ_VALUE to check the multipliers
+		// use frame.getAllTiles to check what letters you have
+		// return the corresponding commandCode from UI
+		// if a play, put the start position and letters into word
+		// if an exchange, put the characters into letters
+		return(UI.COMMAND_PASS);
+	}
+	
+	public Word getWord () {
+		// should not change
+		return(word);
+	}
+		
+	public String getLetters () {
+		// should not change
+		return(letters);
+	}
+}
