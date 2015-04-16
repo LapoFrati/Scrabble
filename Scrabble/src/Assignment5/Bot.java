c59802a94770c10fa20d483c5834c4db0b7aea00
Simone Pignotti <simone.pignotti@ucdconnect.ie>
2015-04-16 02:53:08 +0100

    Add dictionary file for the new code; Temporary change Node code for lower case dictionary file (to be changed); Implementation of a primitive GADDAG structure, still to improve.

--- Scrabble/src/Assignment5/Bot.java
+++ Scrabble/src/Assignment5/Bot.java
@@ -1,11 +1,103 @@
+/* Team: Random2
+ * Members: Lapo Frati 14202439, Simone Pignotti 14202498, Brennan O'Brien 14209388
+ */
+
 package Assignment5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Bot {
 
 	private Word word = new Word();
 	private String letters;
+	private LinkedList<Word> legalWords;
+	private String inputFileName = "sowpods.txt";
 	
-	Bot () {
+	private class GADDAG {
+		
+		private class dagNode {
+			
+			public char letter;
+			public ArrayList<dagNode> children;
+			
+			public dagNode (char letter) {
+				this.letter = letter;
+				children = new ArrayList<dagNode>();
+			}
+			
+			public void addChild (char letter) {
+				children.add(new dagNode(letter));
+			}
+			
+			public int hasChild (char letter) {
+				for (dagNode child : children) {
+					if (child.letter == letter)
+						return children.indexOf(child);
+				}
+				return -1;
+			}
+			
+			public dagNode getLastChild() {
+				return children.get(children.size()-1);
+			}
+			
+			public void print (int tabs) {
+				System.out.println(letter);
+				for (dagNode child : children) {
+					for (int i = 0; i < tabs; i++) {
+						System.out.print('\t');
+					}
+					child.print(tabs+1);
+				}
+			}
+			
+		}
+		
+		private dagNode root;
+		
+		public GADDAG () throws FileNotFoundException {
+			
+			root = new dagNode('@');
+			String word = "";
+			File inputFile = new File(inputFileName);
+			Scanner in = new Scanner(inputFile);
+			int i = 0;
+			while (in.hasNextLine()) {
+				word = in.nextLine();
+				add(root, word);
+				i++;
+				System.out.println(i);
+			}
+			in.close();
+		}
+		
+		public void add(dagNode node, String word) {
+			for (int i = 1; i <= word.length(); i++) {
+				String newWord = new StringBuilder(word.substring(0, i)).reverse().toString() + "@" + word.substring(i);
+				recAdd(node, newWord);
+			}
+		}
+		
+		private void recAdd(dagNode node, String word) {
+			if (word.length() != 0) {
+				node.addChild(word.charAt(0));
+				recAdd(node.getLastChild(), word.substring(1));
+			}
+		}
+		
+		public void print () {
+			root.print(1);
+		}
+		
+	}
+	
+	public Bot () throws FileNotFoundException {
+		GADDAG gad = new GADDAG();
+		gad.print();
 		word.setWord(0, 0, Word.HORIZONTAL, "HELLO");
 		letters = "XYZ";
 	}
